69ded74dbf52c4052ddb5e9a327266a5549c5aba
GraziellaMS21 <graziellamssaavedra06@gmail.com>
2025-09-01 21:45:24 +0800

    Change folder directory of files to myproject

--- viewproduct.php
+++
@@ -1,6 +0,0 @@
-<label for="">Product Name: <?php echo htmlspecialchars($_POST["product_name"]); ?></label>
-<label for="">Category: <?php echo htmlspecialchars($_POST["category"]); ?></label>
-<label for="">Price: <?php echo number_format($_POST["price"], 2); ?></label>
-<label for="">Stock Quantity: <?php echo $_POST["stock_quantity"]; ?></label>
-<label for="">Expiration Date: <?php echo date("M-d-Y", strtotime($_POST["expiration_date"])); ?></label>
-<label for="">Status: <?php echo $_POST["status"]; ?></label>
